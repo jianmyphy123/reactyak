a518d28bc3cf3c33108ba8b6566a286242f4fb89
jianmyphy123 <jianmyphy123@tutanota.com>
2017-08-18 16:39:03 +0200

    construct api zone, comment

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+var ZoneController = require('./ZoneController');
+var CommentContoller = require('./CommentController');
+
+module.exports = {
+  zone: ZoneController,
+  comment: CommentContoller
+}
